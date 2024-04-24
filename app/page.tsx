79e493b922603377b36d97fb069324b863791a7e
jlimsy <limsy.joey@gmail.com>
2024-04-24 22:31:53 +0800

    merge conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,20 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-<<<<<<< HEAD
+
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
-=======
->>>>>>> 8a131ac7b3cf9237c24b1791794fbeed6df50c8c
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-<<<<<<< HEAD
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className={styles.shape} />
-          <p
-            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-          >
-=======
-        {/* <AcmeLogo /> */}
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
->>>>>>> 8a131ac7b3cf9237c24b1791794fbeed6df50c8c
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -43,7 +30,6 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-<<<<<<< HEAD
           <Image
             src="/hero-desktop.png"
             width={1000}
@@ -56,8 +42,6 @@ export default function Page() {
             height={620}
             alt="Screenshots of the dashboard project showing mobile version"
           />
-=======
->>>>>>> 8a131ac7b3cf9237c24b1791794fbeed6df50c8c
         </div>
       </div>
     </main>
