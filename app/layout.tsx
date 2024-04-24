8680a44776af6a42939eb01db16b384fda374ff8
jlimsy <limsy.joey@gmail.com>
2024-04-24 22:28:16 +0800

    remove conflict marker from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
-=======
->>>>>>> 8a131ac7b3cf9237c24b1791794fbeed6df50c8c
 export default function RootLayout({
   children,
 }: {
@@ -11,11 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-<<<<<<< HEAD
       <body className={`${inter.className} antialiased`}>{children}</body>
-=======
-      <body>{children}</body>
->>>>>>> 8a131ac7b3cf9237c24b1791794fbeed6df50c8c
     </html>
   );
 }
